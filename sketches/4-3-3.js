9c8f636320167204591cc733f525b5a7e0da4d9a
winkerVSbecks <varunvachhar@gmail.com>
2020-06-29 10:41:56 -0400

    wip

--- sketches/4-3-3.js
+++ sketches/4-3-3.js
@@ -1,10 +1,11 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math');
 const Color = require('canvas-sketch-util/color');
+const triangulate = require('delaunay-triangulate');
 const clrs = require('../clrs')();
 
 const settings = {
-  dimensions: [600, 600],
+  dimensions: [400, 600],
   animate: true,
   duration: 8,
   scaleToView: true,
@@ -15,13 +16,22 @@ const rect = (context, x, y, width, height, color) => {
   context.fillRect(x, y, width, height);
 };
 
-const circle = (context, x, y, radius, color, lineWidth) => {
-  context.strokeStyle = context.fillStyle = color;
+const player = (context, x, y, radius, stroke, fill = stroke, lineWidth) => {
+  context.strokeStyle = stroke;
+  context.fillStyle = fill;
   context.beginPath();
   context.arc(x, y, radius, 0, Math.PI * 2, false);
   context.lineWidth = lineWidth;
-  if (lineWidth != null) context.stroke();
-  else context.fill();
+  context.fill();
+  context.stroke();
+};
+
+const ball = (context, x, y, radius, stroke, fill = stroke, lineWidth) => {
+  context.fillStyle = fill;
+  context.beginPath();
+  context.arc(x, y, radius, 0, Math.PI * 2, false);
+  context.lineWidth = lineWidth;
+  context.fill();
 };
 
 const goal = (context, x, y, size, color = 'white') => {
@@ -37,6 +47,23 @@ const progress = (context, time, x, y, size, bg, foreground) => {
   context.fillRect(x, y, size * time, 4);
 };
 
+const triangles = (context, triangles, pts, color, lineWidth) => {
+  context.strokeStyle = color;
+  context.lineWidth = lineWidth;
+  context.lineJoin = 'round';
+  context.lineCap = 'round';
+
+  context.beginPath();
+  triangles.forEach(([a, b, c]) => {
+    context.moveTo(...pts[a]);
+    context.lineTo(...pts[b]);
+    context.lineTo(...pts[c]);
+    context.lineTo(...pts[a]);
+  });
+
+  context.stroke();
+};
+
 const sketch = ({ width, height }) => {
   const point = [width * 0.5, height * 0.5];
   const margin = 25;
@@ -47,27 +74,42 @@ const sketch = ({ width, height }) => {
   const tertiary = Color.offsetHSL(background, 0, 0, -20).hex;
 
   const x = (v) => lerp(margin, width - margin, v);
-  const y = (v) => lerp(margin * 3, height - margin * 3, v);
+  const y = (v) => lerp(height - margin * 3, margin * 3, v);
 
   // barcelona formation
   // 4-1-2-3
   // prettier-ignore
   const formation = [
-              [x(0.2), y(0.4)], [x(0.5), y(0.3)], [x(0.8), y(0.45)],
-                  [x(0.375), y(0.55)], [x(0.625), y(0.5)],
-                              [x(0.5), y(0.65)],
-    [x(0.125), y(0.8)], [x(0.375), y(0.8)], [x(0.625), y(0.8)], [x(0.875), y(0.8)],
-                               [x(0.5), y(1)],
+                                   [x(0.5), y(0.6)],
+                          [x(0.2), y(0.5)], [x(0.8), y(0.5)],
+                        [x(0.375), y(0.4)], [x(0.625), y(0.475)],
+                                   [x(0.5), y(0.3)],
+    [x(0.125), y(0.2)], [x(0.375), y(0.2)], [x(0.625), y(0.2)], [x(0.875), y(0.2)],
+                                   [x(0.5), y(0.025)],
   ].reverse();
 
-  const targets = [1, 5, 6, 7, 8, 9].map((x) => formation[x]);
-  targets.push([x(0.5), y(-0.1)]);
+  // prettier-ignore
+  const formationTo = [
+              [x(0.2), y(0.5)], [x(0.5), y(0.6)], [x(0.8), y(0.5)],
+                  [x(0.375), y(0.4)], [x(0.625), y(0.475)],
+                              [x(0.5), y(0.3)],
+    [x(0.125), y(0.2)], [x(0.375), y(0.2)], [x(0.625), y(0.2)], [x(0.875), y(0.2)],
+                                [x(0.5), y(0.025)],
+  ].reverse();
+
+  const targets = [1, 8, 2, 5, 7, 6, 8, 10].map((x) => formation[x]);
+  targets.push([x(0.5), y(1.1)]);
+
+  const formationTriangles = triangulate(formation);
 
   return (props) => {
     const { context, width, height, playhead } = props;
 
     // Fill off-white background
     rect(context, 0, 0, width, height, background);
+
+    // Draw triangles
+    triangles(context, formationTriangles, formation, secondary, 4);
 
     // Draw this scene
     draw(props);
@@ -76,8 +118,8 @@ const sketch = ({ width, height }) => {
     progress(
       context,
       playhead,
-      margin,
-      margin,
+      margin / 2,
+      margin / 2,
       margin * 2,
       secondary,
       foreground
@@ -91,7 +133,7 @@ const sketch = ({ width, height }) => {
   function draw({ context, playhead, deltaTime }) {
     // Chosoe size of circle & stroke
     const lineWidth = 4;
-    const radius = 15;
+    const radius = 10;
 
     // Choose one of the N targets based on loop time
     const targetIndex = Math.floor(playhead * targets.length);
@@ -99,7 +141,7 @@ const sketch = ({ width, height }) => {
 
     // Draw the start and end point
     formation.forEach((p) => {
-      circle(context, p[0], p[1], radius, foreground, lineWidth);
+      player(context, p[0], p[1], radius, foreground, background, lineWidth);
     });
 
     // Determine a rate at which we will step forward each frame,
@@ -110,8 +152,8 @@ const sketch = ({ width, height }) => {
     point[0] = lerp(point[0], target[0], rate);
     point[1] = lerp(point[1], target[1], rate);
 
-    // Draw current point
-    circle(context, point[0], point[1], radius / 2, foreground);
+    // Draw ball
+    ball(context, point[0], point[1], radius / 2, foreground);
   }
 };
 
